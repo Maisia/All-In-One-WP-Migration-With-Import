5d32572ab59cd9e732425bbec34f61c7169020ca
Maisia <36457009+Maisia@users.noreply.github.com>
2022-12-12 13:05:40 +0200

    Update class-ai1wm-database-mysqli.php
    This should fix warning 
    PHP Warning:  mysqli_query(): Empty query in /var/www/wptbox/wp-content/plugins/all-in-one-wp-migration/lib/vendor/servmask/database/class-ai1wm-database-mysqli.php on line 35

--- lib/vendor/servmask/database/class-ai1wm-database-mysqli.php
+++ lib/vendor/servmask/database/class-ai1wm-database-mysqli.php
@@ -31,9 +31,36 @@ class Ai1wm_Database_Mysqli extends Ai1wm_Database {
 	 * @param  string   $input SQL query
 	 * @return resource
 	 */
-	public function query( $input ) {
-		return mysqli_query( $this->wpdb->dbh, $input, MYSQLI_STORE_RESULT );
-	}
+        public function query( $input ) {
+                if ( ! mysqli_real_query( $this->wpdb->dbh, $input ) ) {
+                        $mysqli_errno = 0;
+
+                        // Get MySQL error code
+                        if ( ! empty( $this->wpdb->dbh ) ) {
+                                if ( $this->wpdb->dbh instanceof mysqli ) {
+                                        $mysqli_errno = mysqli_errno( $this->wpdb->dbh );
+                                } else {
+                                        $mysqli_errno = 2006;
+                                }
+                        }
+
+                        // MySQL server has gone away, try to reconnect
+                        if ( empty( $this->wpdb->dbh ) || 2006 === $mysqli_errno ) {
+                                if ( ! $this->wpdb->check_connection( false ) ) {
+                                        throw new Ai1wm_Database_Exception( __( 'Error reconnecting to the database. <a href="https://help.servmask.com/knowledgebase/mysql-error-reconnecting/" target="_blank">Technical details</a>', AI1WM_PLUGIN_NAME ), 503 );
+                                }
+
+                                mysqli_real_query( $this->wpdb->dbh, $input );
+                        }
+                }
+
+                // Copy results from the internal mysqlnd buffer into the PHP variables fetched
+                if ( defined( 'MYSQLI_STORE_RESULT_COPY_DATA' ) ) {
+                        return mysqli_store_result( $this->wpdb->dbh, MYSQLI_STORE_RESULT_COPY_DATA );
+                }
+
+                return mysqli_store_result( $this->wpdb->dbh );
+        }
 
 	/**
 	 * Escape string input for mysql query
